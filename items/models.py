ec571ce6240d5ac7d5b7705832ed8943042913fb
pazdaninho <p4zdaninho.cr@gmail.com>
2022-07-28 13:17:43 +0200

    create db and superuser

--- items/models.py
+++ items/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=200)
+    last_usage = models.DateTimeField('date_published')
+
+    def __str__(self):
+        return self.category_name
+
+    def used_last_year(self):
+        return self.last_usage >= timezone.now() - datetime.timedelta(days=365)
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.item_name
