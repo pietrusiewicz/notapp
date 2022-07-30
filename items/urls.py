8d7bdb00f6b79a9d98b1d0eef490a394fdb984e6
pazdaninho <p4zdaninho.cr@gmail.com>
2022-07-30 09:54:12 +0200

    end docs part 3

--- items/urls.py
+++ items/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'items'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:category_id>/', views.items, name='items'),
