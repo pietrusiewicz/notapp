00082e0aac4a225f9fd0454bd9a908dcdc1c8e7d
pazdaninho <p4zdaninho.cr@gmail.com>
2022-07-31 10:29:57 +0200

    create incrementing price after execute form

--- items/urls.py
+++ items/urls.py
@@ -6,4 +6,6 @@ app_name = 'items'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:category_id>/', views.items, name='items'),
+    path('<int:category_id>/results/', views.results, name='results'),
+    path('<int:category_id>/order/', views.order, name='order'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-#from django.template import loader
+from django.urls import reverse
 
-from .models import Category
+from .models import Category, Item
 
 def index(r):
     latest_categories_list = Category.objects.order_by('-last_usage')[:5]
@@ -11,7 +12,26 @@ def index(r):
     return render(r, 'items/index.html', context)
 
 def items(r, category_id):
-    item = get_object_or_404(Category, pk=category_id)
-    response = f"You're looking at items in category {category_id}"
-    return render(r, 'items/items.html', {'item':item})
+    cat = get_object_or_404(Category, pk=category_id)
+    return render(r, 'items/items.html', {'category':cat})
 
+def results(r, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    return render(r, 'items/results.html', {'category': category})
+
+def order(r, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    try:
+        print(r.POST)
+        selected_items = r.POST['item']
+    except (KeyError, Item):
+        return render(r, 'items/items.html', {
+            'category': category,
+            'error_message': "You picked nothing",
+        })
+    else:
+        for item in category.item_set.all():
+            if str(item.pk) in selected_items:
+                item.price += 0.01
+                item.save()
+        return HttpResponseRedirect(reverse('items:results', args=(category_id,)))
