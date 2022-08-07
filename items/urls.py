1821f1263957fedffbe0ccea86ede3b2ebd84728
pazdaninho <p4zdaninho.cr@gmail.com>
2022-08-07 11:55:13 +0200

    repair display template profile

--- items/urls.py
+++ items/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.index.as_view(), name='index'),
     path('<int:category_id>/', views.items.as_view(), name='items'),
     path('<int:category_id>/results/', views.results.as_view(), name='results'),
+    path('profile/', views.profile, name='profile'),
     path('<int:category_id>/order/', views.order, name='order'),
     path('login/', views.login.as_view(), name='login'),
     path('auth/', views.auth, name='auth'),
--- items/views.py
+++ items/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 from django import forms
@@ -31,6 +31,25 @@ class results(TemplateView):
         cat = get_object_or_404(Category, pk=cid)
         return {'category': cat}
 
+"""
+class profile(TemplateView):
+    template_name = 'items/profile.html'
+
+    def get_context_data(self, **kwargs):
+        #print(self.kwargs)
+        user= self.kwargs['user']
+        return {'user':user, 'orders':Order.objects.get(pk=user.id)}
+"""
+def profile(r):
+    from django.contrib.auth.models import User
+    user = User.objects.get(username=r.session['usname'])
+    orders = Order.objects.get(pk=user.id) if Order.objects.count() > 0 else "We haven't orders"
+    context = {
+            'usname': user.username, 
+            'orders': orders
+    }
+    return render(r, 'items/profile.html', context)
+
 class login(TemplateView):
     template_name = 'items/login.html'
 
@@ -43,7 +62,21 @@ def auth(r):
     # correct password
     if user:
         #TODO
-        return ""
+        #user_obj = User.query.filter_by(username=user)
+        r.session['usname'] = str(user)
+        #print(r.session.get('usname'))
+        print(r.session.__dir__())
+        print(r.session.keys())
+        print(r.session.values())
+        print(r.session.items())
+        #r.session.save()
+        """
+        response = HttpResponse(status=302)
+        response['Location'] = '/sklapp/profile/'
+        return response
+        """
+        return HttpResponseRedirect('/sklapp/profile/')
+        #return HttpResponseRedirect(reverse('items:profile', args=(user,)))
     # wrong password
     else:
         return render(r, 'items/login.html', {"error_message": "Wrong Password"})
