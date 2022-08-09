4fa9b9d9fa7648992c4bc1391e7ba9fcfe51bef5
pazdaninho <p4zdaninho.cr@gmail.com>
2022-08-09 15:53:56 +0200

    create logout procedure

--- items/views.py
+++ items/views.py
@@ -31,38 +31,26 @@ class results(TemplateView):
         cat = get_object_or_404(Category, pk=cid)
         return {'category': cat}
 
-"""
-class profile(TemplateView):
-    template_name = 'items/profile.html'
-
-    def get_context_data(self, **kwargs):
-        #print(self.kwargs)
-        user= self.kwargs['user']
-        return {'user':user, 'orders':Order.objects.get(pk=user.id)}
-"""
 def profile(r):
     from django.contrib.auth.models import User
-    user = User.objects.get(username=r.session['usname'])
-    orders = Order.objects.get(pk=user.id) if Order.objects.count() > 0 else "We haven't orders"
-    context = {
-            'usname': user.username, 
-            'orders': orders
-    }
-    return render(r, 'items/profile.html', context)
+    if 'usname' in r.session.keys():
+        user = User.objects.get(username=r.session['usname'])
+        orders = Order.objects.get(pk=user.id) if Order.objects.count() > 0 else "We haven't orders"
+        context = {
+                'usname': user.username, 
+                'orders': orders
+        }
+        return render(r, 'items/profile.html', context)
 
 class login(TemplateView):
     template_name = 'items/login.html'
 
 def auth(r):
     from django.contrib.auth import authenticate
-    #print(r.POST)
     usname, passwd = r.POST['usname'],r.POST['passwd']
     user = authenticate(username=usname, password=passwd)
-    #print(user)
     # correct password
     if user:
-        #TODO
-        #user_obj = User.query.filter_by(username=user)
         r.session['usname'] = str(user)
         return HttpResponseRedirect('/sklapp/profile/')
     # wrong password
@@ -74,13 +62,19 @@ class register(TemplateView):
 
 def create_user(r):
     from django.contrib.auth.models import User
-    username, email, password = r.POST['register']
+    print(r.POST)
+    username, email, password = [r.POST[s] for s in ['usname','passwd','email']]
     if username in User.objects.all():
         return render(r, 'items/register.html', {"error_message": "User exists"})
-    # created user
+    # create an user
     else:
         user = User.objects.create_user(username, email, password)
         user.save()
+        return HttpResponseRedirect('/sklapp/login/')
+
+def logout(r):
+    r.session['usname']=''
+    return HttpResponseRedirect('/sklapp/login/')
 
 def order(r, category_id):
     category = get_object_or_404(Category, pk=category_id)
